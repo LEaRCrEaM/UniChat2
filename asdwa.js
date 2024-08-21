d699cc8fbaf43d68486391433558364685d35b15
LEaRCrEaM <127627494+LEaRCrEaM@users.noreply.github.com>
2024-08-21 15:32:04 -0400

    Update asdwa.js

--- asdwa.js
+++ asdwa.js
@@ -37,7 +37,7 @@
                 } else {
                     User.name = t.textContent.trim();
                 };
-                if (![].includes(User.name)) {
+                if (!['Meteron'].includes(User.name)) {
                     alert(`${User.name} does not have permission to use this hack!`);
                     for (const k in window) {
                         window[k] = 't';
