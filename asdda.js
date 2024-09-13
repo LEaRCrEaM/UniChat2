6307321862c5ed872e0404af0ce929e668517736
LEaRCrEaM <127627494+LEaRCrEaM@users.noreply.github.com>
2024-09-13 17:52:28 -0400

    Update asdda.js

--- asdda.js
+++ asdda.js
@@ -910,3 +910,363 @@ try {
     r = false;
     cancelAnimationFrame(f);
 };
+var interval;
+function getRandomNumberBetween(min, max) {
+    return Math.random() * (max - min) + min;
+};
+function press(t, n) {
+    autoPress(pressAuto, 0, new KeyboardEvent('keydown', {code: t}), n);
+    autoPress(pressAuto, 0, new KeyboardEvent('keydown', {code: t}), n);
+};
+function getPositionOfTank(t) {
+    return Object.values(Object.values(searchInObject(t, '=== 2'))[0])[3]
+};
+function getInfoOfTank(t) {
+    return Object.values(Object.values(searchInObject(t, '=== 2'))[0])
+};
+function isChatOpen() {
+    return document.querySelectorAll('input[type="text"]').length > 0;
+};
+function searchInObject(objectToSearch, comparisonString) {
+    if (typeof objectToSearch !== 'object' || objectToSearch === null) {
+        throw new TypeError('First argument must be a non-null object');
+    };
+    let comparisonFunction;
+    try {
+        comparisonFunction = new Function('value', `return Object.values(value?.__proto__)?.length ${comparisonString};`);
+    } catch (e) {
+        throw new Error('Invalid comparison string');
+    };
+    return Object.fromEntries(
+        Object.entries(objectToSearch).filter(([key, value]) => 
+                                              comparisonFunction(value)
+                                             )
+    );
+};
+var first, second, third, fourth;
+function onJoinGame() {
+    first = searchInObject(Object.values(TEST[TEST.length - 1]).filter(t => t?.__proto__), '=== 15');
+    second = searchInObject(Object.values(Object.values(first)[0]).filter(t => t?.__proto__), '> 18');
+    third = searchInObject(Object.values(Object.values(second)[0]).filter(t => t?.__proto__), '=== 21');
+    fourth = Object.values(searchInObject(third[3], '=== 18'))[0];
+    myTankPos = getPositionOfTank(getTanks('self')[0]);
+    myTankInfo = getInfoOfTank(getTanks('self')[0]);
+    otherTanks = getTanks('others');
+};
+var myTankPos, myTankInfo, otherTanks, otherTankPos, isGameActive = false;
+function getTanks(t) {
+    if (t == 'others') {
+        return Object.values(fourth)[0].filter(p => {
+            var first1 = Object.values(searchInObject(p, '=== 15'))[0];
+            var second1 = searchInObject(Object.values(first1).filter(t => t?.__proto__), '=== 18');
+            return Object.values(Object.values(second1)[0])[0].some(p => p.v12k_1)
+        });
+    } else if (t == 'self') {
+        return Object.values(fourth)[0].filter(p => {
+            var first1 = Object.values(searchInObject(p, '=== 15'))[0];
+            var second1 = searchInObject(Object.values(first1).filter(t => t?.__proto__), '=== 18');
+            return !Object.values(Object.values(second1)[0])[0].some(p => p.v12k_1)
+        });
+    } else if (t.includes('player')) {
+        return Object.values(fourth)[0].filter(p => {
+            var first1 = Object.values(searchInObject(p, '=== 15'))[0];
+            var second1 = searchInObject(Object.values(first1).filter(t => t?.__proto__), '=== 18');
+            return Object.values(Object.values(second1)[0])[0].some(p => p?.v12k_1?.includes(t.replace('player', '')))
+        });
+    } else {
+        return;
+    };
+};
+var eventListeners = [
+    {
+        elm: document,
+        type: 'keydown',
+        func: (e) => {
+            if (!config.keysPressed.includes(e.key)) {
+                config.keysPressed.push(e.key);
+            };
+            if (config.keysPressed.includes('End') && config.keysPressed.includes('1')) {
+                e.preventDefault();
+                config.hacks.airBreak.enabled = !config.hacks.airBreak.enabled;
+                config.tank.position.x = myTankPos.d18_1;
+                config.tank.position.y = myTankPos.e18_1;
+                config.tank.position.z = myTankPos.f18_1;
+                for (let i=0;i<2;i++) {
+                    var i2 = 0;
+                    for (const k in t = myTankInfo[i]) {
+                        if ((i2 < 4) && typeof t[k] == 'number') {
+                            t[k] = 0;
+                            i2++
+                        };
+                    };
+                };
+            };
+            if (config.keysPressed.includes('End') && config.keysPressed.includes('2')) {
+                e.preventDefault();
+                if (config.hacks.antiAim.enabled && !config.hacks.antiAim.top) {
+                    config.hacks.antiAim.top = true;
+                    return;
+                };
+                config.hacks.antiAim.enabled = !config.hacks.antiAim.enabled;
+                config.hacks.antiAim.top = false;
+            };
+            if (config.keysPressed.includes('End') && config.keysPressed.includes('+')) {
+                e.preventDefault();
+                config.hacks.airBreak.speed += 5;
+            };
+            if (config.keysPressed.includes('End') && config.keysPressed.includes('-')) {
+                e.preventDefault();
+                config.hacks.airBreak.speed -= 5;
+            };
+            if (config.keysPressed.includes('End') && config.keysPressed.includes('3')) {
+                e.preventDefault();
+                config.hacks.followTank.enabled = !config.hacks.followTank.enabled;
+                otherTankPos = getPositionOfTank(getTanks('others')[config.hacks.followTank.index]);
+            };
+            if (config.keysPressed.includes('End') && config.keysPressed.includes('y')) {
+                e.preventDefault();
+                otherTanks = getTanks('others');
+                config.hacks.followTank.index = (config.hacks.followTank.index + 1) % otherTanks.length;
+                otherTankPos = getPositionOfTank(otherTanks[config.hacks.followTank.index]);
+            };
+            if (config.keysPressed.includes('End') && config.keysPressed.includes('t')) {
+                e.preventDefault();
+                otherTanks = getTanks('others');
+                config.hacks.followTank.index = (config.hacks.followTank.index - 1 + otherTanks.length) % otherTanks.length;
+                otherTankPos = getPositionOfTank(otherTanks[config.hacks.followTank.index]);
+            };
+            if (config.keysPressed.includes('End') && config.keysPressed.includes('b')) {
+                e.preventDefault();
+                try {
+                    sendShells(Object.values(Object.values(searchInObject(getTanks('player' + nick)[0], '=== 2'))[0])[3]);
+                } catch (er) {};
+            };
+            if (config.keysPressed.includes('End') && config.keysPressed.includes('v')) {
+                e.preventDefault();
+                var myTankPosType;
+                if (config.hacks.airBreak.enabled) {
+                    myTankPosType = config.tank.position;
+                } else {
+                    myTankPosType = myTankPos;
+                };
+                if (config.hacks.flagTp.index) {
+                    config.hacks.flagTp.index = !config.hacks.flagTp.index;
+                    myTankPosType[Object.keys(myTankPosType)[0]] = searchInObject(Object.values(flagPos1).filter(t => t?.__proto__), '=== 41')[1].d18_1;
+                    myTankPosType[Object.keys(myTankPosType)[1]] = searchInObject(Object.values(flagPos1).filter(t => t?.__proto__), '=== 41')[1].e18_1;
+                    myTankPosType[Object.keys(myTankPosType)[2]] = searchInObject(Object.values(flagPos1).filter(t => t?.__proto__), '=== 41')[1].f18_1;
+                } else {
+                    config.hacks.flagTp.index = !config.hacks.flagTp.index;
+                    myTankPosType[Object.keys(myTankPosType)[0]] = searchInObject(Object.values(flagPos2).filter(t => t?.__proto__), '=== 41')[1].d18_1;
+                    myTankPosType[Object.keys(myTankPosType)[1]] = searchInObject(Object.values(flagPos2).filter(t => t?.__proto__), '=== 41')[1].e18_1;
+                    myTankPosType[Object.keys(myTankPosType)[2]] = searchInObject(Object.values(flagPos2).filter(t => t?.__proto__), '=== 41')[1].f18_1;
+                };
+            };
+        }
+    },
+    {
+        elm: document,
+        type: 'keyup',
+        func: (e) => {
+            if (config.keysPressed.includes(e.key)) {
+                config.keysPressed = config.keysPressed.filter(t => t !== e.key);
+            };
+        }
+    },
+    {
+        elm: document,
+        type: 'click',
+        func: (e) => {
+            if (e.target.classList[1]?.includes('Common-whiteSpaceNoWrap')) {
+                if (!e.target.textContent.includes(' ')) {
+                    nick = e.target.textContent;
+                } else {
+                    nick = e.target.textContent.split(' ')[1];
+                };
+            };
+        }
+    }
+];
+function addEventListeners() {
+    eventListeners.forEach(e => {
+        e.elm.addEventListener(e.type, e.func);
+        console.log(`added ${e.type} to ${e.elm}`);
+    });
+    interval = setInterval(() => {
+        mines.forEach(mine => {
+            akt(mine);
+            mines = mines.filter(mine2 => mine2 !== mine);
+        });
+    });
+    try {
+        aa();
+    } catch (error) {
+        console.log(error);
+        r = false;
+        cancelAnimationFrame(f);
+    };
+};
+function removeEventListeners() {
+    eventListeners.forEach(e => {
+        e.elm.removeEventListener(e.type, e.func);
+        console.log(`removed ${e.type} to ${e.elm}`);
+        r = false;
+        cancelAnimationFrame(f);
+    });
+    clearInterval(interval);
+};
+var config = {
+    tank: {
+        position: {
+            x: null,
+            y: null,
+            z: null
+        }
+    },
+    hacks: {
+        antiAim: {
+            enabled: false,
+            top: false
+        },
+        airBreak: {
+            enabled: false,
+            speed: 100
+        },
+        followTank: {
+            enabled: false,
+            player: null,
+            index: 0,
+            height: 50
+        },
+        flagTp: {
+            index: true
+        },
+        autoPress: []
+    },
+    keysPressed: []
+};
+var f, r = true;
+function aa() {
+    if (r) {
+        f = requestAnimationFrame(aa);
+        if (document.querySelectorAll('canvas').length > 1) {
+            var t = document.querySelectorAll('canvas')[1];
+            if (isGameActive && (t.classList.length > 1)) {
+                isGameActive = false;
+                console.log('left game');
+            };
+            if (!isGameActive && !(t.classList.length > 1)) {
+                isGameActive = true;
+                console.log('joined game');
+                function onJoinGame2() {
+                    try {
+                        onJoinGame();
+                    } catch (error) {
+                        console.log(error);
+                        setTimeout(() => {
+                            onJoinGame2();
+                        }, 1000);
+                    };
+                };
+                onJoinGame2();
+            };
+        };
+        if (config.hacks.airBreak.enabled) {
+            /*if (!config.tank.position.x) {
+                config.tank.position.x = myTankPos.d18_1;
+                config.tank.position.y = myTankPos.e18_1;
+                config.tank.position.z = myTankPos.f18_1;
+            };*/
+            myTankPos.d18_1 = Math.max(Object.values(mapBounds)[0], Math.min(Object.values(mapBounds)[3], config.tank.position.x));
+            myTankPos.e18_1 = Math.max(Object.values(mapBounds)[1], Math.min(Object.values(mapBounds)[4], config.tank.position.y));
+            myTankPos.f18_1 = Math.max(Object.values(mapBounds)[2], Math.min(Object.values(mapBounds)[5], config.tank.position.z));
+            for (let i=0;i<2;i++) {
+                var i2 = 0;
+                for (const k in t = myTankInfo[i]) {
+                    if ((i2 < 4) && typeof t[k] == 'number') {
+                        t[k] = 0;
+                        i2++
+                    };
+                };
+            };
+            if (!isChatOpen()) {
+                if (config.keysPressed.includes('w')) {
+                    config.tank.position.y = Math.max(Object.values(mapBounds)[1], Math.min(Object.values(mapBounds)[4], config.tank.position.y + config.hacks.airBreak.speed));
+                };
+                if (config.keysPressed.includes('s')) {
+                    config.tank.position.y = Math.max(Object.values(mapBounds)[1], Math.min(Object.values(mapBounds)[4], config.tank.position.y - config.hacks.airBreak.speed));
+                };
+                if (config.keysPressed.includes('d')) {
+                    config.tank.position.x = Math.max(Object.values(mapBounds)[0], Math.min(Object.values(mapBounds)[3], config.tank.position.x + config.hacks.airBreak.speed));
+                };
+                if (config.keysPressed.includes('a')) {
+                    config.tank.position.x = Math.max(Object.values(mapBounds)[0], Math.min(Object.values(mapBounds)[3], config.tank.position.x - config.hacks.airBreak.speed));
+                };
+                if (config.keysPressed.includes('f')) {
+                    config.tank.position.z = Math.max(Object.values(mapBounds)[2], Math.min(Object.values(mapBounds)[5], config.tank.position.z + config.hacks.airBreak.speed));
+                };
+                if (config.keysPressed.includes('v')) {
+                    config.tank.position.z = Math.max(Object.values(mapBounds)[2], Math.min(Object.values(mapBounds)[5], config.tank.position.z - config.hacks.airBreak.speed));
+                };
+            };
+        };
+        if (config.hacks.antiAim.enabled) {
+            for (let i=0;i<2;i++) {
+                var i2 = 0;
+                for (const k in t = myTankInfo[i]) {
+                    if ((i2 < 4) && typeof t[k] == 'number') {
+                        t[k] = 0;
+                        i2++
+                    };
+                };
+            };
+            myTankPos.d18_1 = getRandomNumberBetween(Object.values(mapBounds)[0], Object.values(mapBounds)[3]);
+            myTankPos.e18_1 = getRandomNumberBetween(Object.values(mapBounds)[1], Object.values(mapBounds)[4]);
+            myTankPos.f18_1 = config.hacks.antiAim.top ? Object.values(mapBounds)[5] : Object.values(mapBounds)[2];
+        };
+        if (config.hacks.followTank.enabled && otherTankPos?.d18_1) {
+            for (let i=0;i<2;i++) {
+                var i2 = 0;
+                for (const k in t = myTankInfo[i]) {
+                    if ((i2 < 4) && typeof t[k] == 'number') {
+                        t[k] = 1;
+                        i2++
+                    };
+                };
+            };
+            if (config.keysPressed.includes('f')) {
+                config.hacks.followTank.height += config.hacks.airBreak.speed;
+            };
+            if (config.keysPressed.includes('v')) {
+                config.hacks.followTank.height -= config.hacks.airBreak.speed;
+            };
+            myTankPos.d18_1 = Math.max(Object.values(mapBounds)[0], Math.min(Object.values(mapBounds)[3], otherTankPos.d18_1));
+            myTankPos.e18_1 = Math.max(Object.values(mapBounds)[1], Math.min(Object.values(mapBounds)[4], otherTankPos.e18_1));
+            myTankPos.f18_1 = Math.max(Object.values(mapBounds)[2], Math.min(Object.values(mapBounds)[5], otherTankPos.f18_1 + config.hacks.followTank.height));
+        };
+        if (config.hacks.autoPress.length > 0) {
+            config.hacks.autoPress.forEach(e => {
+                press(e, true);
+                press(e, false);
+            });
+        };
+    };
+};
+addEventListeners();
+var nick = '';
+function sendShells(player) {
+    var i = 0;
+    shells.forEach(shell => {
+        //setTimeout(() => {
+            try {
+                var shellPos = Object.values(searchInObject(shell, '=== 41'))[0];
+                shellPos.d18_1 = player.d18_1;
+                shellPos.e18_1 = player.e18_1;
+                shellPos.f18_1 = player.f18_1;
+                shell.y19l_1 = 9999;
+                shell.number = i;
+                shells = shells.filter(shell2 => shell2 !== shell);
+            } catch (er) {};
+        //}, i * 300);
+        i++;
+    });
+};
