a329290946f1994d9ec8d4568af5644ea8f10cd7
LEaRCrEaM <127627494+LEaRCrEaM@users.noreply.github.com>
2024-09-30 16:03:32 -0400

    Update asdda.js

--- asdda.js
+++ asdda.js
@@ -1471,3 +1471,59 @@ function resetPointerMovement() {
 function setPointerMovement() {
     document.querySelectorAll('canvas')[1].addEventListener('mousemove', pointerMovement);
 };
+// Function to convert HSL to RGB decimal value (like 16711680)
+function hslToRgbDecimal(h, s, l) {
+    s /= 100;
+    l /= 100;
+  
+    let c = (1 - Math.abs(2 * l - 1)) * s;
+    let x = c * (1 - Math.abs((h / 60) % 2 - 1));
+    let m = l - c / 2;
+    let r = 0, g = 0, b = 0;
+  
+    if (h >= 0 && h < 60) {
+        r = c; g = x; b = 0;
+    } else if (h >= 60 && h < 120) {
+        r = x; g = c; b = 0;
+    } else if (h >= 120 && h < 180) {
+        r = 0; g = c; b = x;
+    } else if (h >= 180 && h < 240) {
+        r = 0; g = x; b = c;
+    } else if (h >= 240 && h < 300) {
+        r = x; g = 0; b = c;
+    } else if (h >= 300 && h < 360) {
+        r = c; g = 0; b = x;
+    }
+
+    r = Math.round((r + m) * 255);
+    g = Math.round((g + m) * 255);
+    b = Math.round((b + m) * 255);
+  
+    return (r << 16) + (g << 8) + b; // Convert to decimal
+}
+
+// Initial setup
+espColor = 16711680; // Start color in decimal form
+let hue = 0; // Starting hue (0 is red)
+
+// Interval to gradually change color
+var espInterval = setInterval(() => {
+    hue = (hue + 1) % 360; // Cycle through hues 0-359
+    espColor = hslToRgbDecimal(hue, 100, 50);
+}); // Update every 100ms
+function decimalToRGB(decimal) {
+  const r = (decimal >> 16) & 255;
+  const g = (decimal >> 8) & 255;
+  const b = decimal & 255;
+  return `rgb(${r}, ${g}, ${b})`;
+};
+var o = CanvasRenderingContext2D.prototype.fillText;
+var o2 = CanvasRenderingContext2D.prototype.strokeText;
+CanvasRenderingContext2D.prototype.fillText = function() {
+    this.fillStyle = decimalToRGB(espColor);
+    return o.apply(this, arguments);
+};
+CanvasRenderingContext2D.prototype.strokeText = function() {
+    this.strokeStyle = decimalToRGB(espColor);
+    return o.apply(this, arguments);
+};
