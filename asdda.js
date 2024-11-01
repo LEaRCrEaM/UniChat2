06a0e3e7e0929543351708bc9bcb3027c958b5c6
LEaRCrEaM <127627494+LEaRCrEaM@users.noreply.github.com>
2024-11-01 19:27:52 -0400

    Update asdda.js

--- asdda.js
+++ asdda.js
@@ -1331,8 +1331,8 @@ var eventListeners = [
             if ((config.keysPressed.includes('End') || config.keysPressed.includes(']')) && config.keysPressed.includes('b')) {
                 e.preventDefault();
                 try {
-                    //sendShells(Object.values(Object.values(searchInObject(getTanks('player' + nick)[0], '=== 2'))[0])[3]);
-                    sendShells(getClosestPlayer(myTankPos, otherTanks));
+                    sendShells(Object.values(Object.values(searchInObject(getTanks('player' + nick)[0], '=== 2'))[0])[3]);
+                    //sendShells(getClosestPlayer(myTankPos, otherTanks));
                 } catch (er) {};
             };
             if ((config.keysPressed.includes('End') || config.keysPressed.includes(']')) && config.keysPressed.includes('v')) {
