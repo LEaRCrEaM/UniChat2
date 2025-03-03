ac8f71cb5ebac0da09c08351216e7b8cd0b579eb
LEaRCrEaM <127627494+LEaRCrEaM@users.noreply.github.com>
2025-03-02 19:37:08 -0500

    Update asdda.js

--- asdda.js
+++ asdda.js
@@ -1740,7 +1740,7 @@ function aa() {
             });
         };
         if (config.hacks.noClip.enabled && isGameActive) {
-            var body = Object.values(searchInObject(Object.values(searchInObject(Object.values(searchInObject(getTanks('self')[0], '==14'))[0], '==3'))[0], '==43'))[0];
+            var body = Object.values(searchInObject(Object.values(searchInObject(Object.values(searchInObject(myTank, '==14'))[0], '==3'))[0], '==43'))[0];
             if (!body) return;
             if (body.scaled) return;
             for (const k in body) {
