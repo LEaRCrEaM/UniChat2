b37f82170a8bf0a0bf33764fdb740ee219fa00ac
LEaRCrEaM <127627494+LEaRCrEaM@users.noreply.github.com>
2025-06-20 12:25:26 -0400

    Update asdda.js

--- asdda.js
+++ asdda.js
@@ -2061,9 +2061,9 @@ function setVars() {
 var Tanki = {
     get allTanks() {
         if (!Utils.cameraComponent) return;
-        var t = Object.values(Object.values(searchInObject(Object.values(searchInObject(Object.values(searchInObject(Object.values(searchInObject(Utils.cameraComponent, '==15'))[0], '>50'))[0], '==21'))[0], '==18'))[0])[0];
+        var t = Object.values(Object.values(searchInObject(Object.values(searchInObject(Object.values(searchInObject(Object.values(searchInObject(Utils.cameraComponent, '==15'))[0], '>50'))[0], '==20'))[0], '==17'))[0])[0];
         for (let i=0;i<t.length;i++) {
-            t[i].espInfo = Object.values(searchInObject(Object.values(Object.values(searchInObject(Object.values(searchInObject(t[i], '==15'))[0], '==18'))[0])[0], '==2'))[0]
+            t[i].espInfo = Object.values(searchInObject(Object.values(Object.values(searchInObject(Object.values(searchInObject(t[i], '==15'))[0], '==17'))[0])[0], '==2'))[0]
         };
         return t;
     },
