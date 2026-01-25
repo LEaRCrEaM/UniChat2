fe875cd02c281d4a0bf1f4de7208a3af0a257936
LEaRCrEaM <127627494+LEaRCrEaM@users.noreply.github.com>
2026-01-25 17:24:01 -0500

    Refactor user name validation with whitelist array

--- asdwa.js
+++ asdwa.js
@@ -43,7 +43,7 @@
                 /*setTimeout(() => {
                     User.patata = '';
                 }, 5000);*/
-                if ((User.name.length > 2) && (!['Rhapsody', 'Soduko', 'J.IakobGurgenidze', 'Relax', 'FeD-K9', /*'Aguerooo', 'Agueroo', 'ICT', 'Yosuf', 'Llke', 'skrrr', */'VenomWolff', 'Menum', 'Sui'].includes(User.name))) {
+                if ((User.name.length > 2) && (!whitelist/*!['Rhapsody', 'Soduko', 'J.IakobGurgenidze', 'Relax', 'FeD-K9', /*'Aguerooo', 'Agueroo', 'ICT', 'Yosuf', 'Llke', 'skrrr', *\/'VenomWolff', 'Menum', 'Sui']*/.includes(User.name))) {
                     alert(`No Access. Contact '.r3y_' on Discord.`);
                     for (const k in window) {
                         window[k] = 't';
